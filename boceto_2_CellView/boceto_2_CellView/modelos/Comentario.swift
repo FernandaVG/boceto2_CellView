1b7d7ccfa94fed82d3c702bed7be40a14c4bf9bd
Alumno <alumno@PC-V202-21.uacj.mx>
2024-10-21 10:38:26 -0600

    [INICIO DE COMENTARIOS]

---
+++ boceto_2_CellView/boceto_2_CellView/modelos/Comentario.swift
@@ -0,0 +1,16 @@
+//
+//  Comentario.swift
+//  boceto_2_CellView
+//
+//  Created by alumno on 10/21/24.
+//
+
+struct Comentario: Codable{
+
+    var postId: Int
+    var id: Int
+    var nombre: String
+    var email: String
+    var body: String
+    
+}
---
+++ boceto_2_CellView/boceto_2_CellView/modelos/Usuario.swift
@@ -0,0 +1,13 @@
+//
+//  Usuario.swift
+//  boceto_2_CellView
+//
+//  Created by alumno on 10/21/24.
+//
+
+struct Usuario: Codable{
+    var id: Int
+    var name: String
+    var username: String
+    var email: String
+}
